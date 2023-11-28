d958513abd2ffe3b3253ada94f9cbda8ab7ca23d
Bryan Carlos Martinez Elizalde <55309306+bryanmartineze@users.noreply.github.com>
2023-11-28 14:58:14 -0600

    updated code

--- vmswithbackupscript-console-format.sh
+++ vmswithbackupscript-console-format.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
 
-# Get a list of all subscriptions
-subscriptions=$(az account list --all --query '[].id' --output tsv)
+# Output CSV header
+echo "VMName,BackupEnabled,SubscriptionName"
 
-# Iterate through each subscription
-while read -r subscriptionId; do
-    echo "Checking VMs in subscription: $subscriptionId"
-    
-    # Set the current subscription
-    az account set --subscription "$subscriptionId"
+# Get the list of subscription IDs and names
+subscriptions=$(az account list --query "[].{id:id,name:name,state:state}" -o json | jq -r '.[] | select(.state == "Enabled") | "\(.id),\(.name)"')
+
+# Iterate over each subscription
+IFS=$'\n'
+for subscription in $subscriptions; do
+    subscription_id=$(echo "$subscription" | cut -d',' -f1)
+    subscription_name=$(echo "$subscription" | cut -d',' -f2)
+
+    # Set the current subscription context
+    az account set --subscription "$subscription_id"
 
     # Get a list of all resource groups in the current subscription
     resourceGroups=$(az group list --query '[].name' --output tsv)
 
     # Iterate through each resource group
     while read -r resourceGroup; do
-        echo "Checking VMs in resource group: $resourceGroup"
-
         # Get a list of all VMs in the current resource group
         vms=$(az vm list --resource-group "$resourceGroup" --query '[].name' --output tsv)
 
         # Iterate through each VM
         while read -r vmName; do
-            echo "Checking backup status for VM: $vmName"
-
             # Get backup status for the VM
-            backupStatus=$(az backup protection check-vm --vm-id "/subscriptions/$subscriptionId/resourceGroups/$resourceGroup/providers/Microsoft.Compute/virtualMachines/$vmName" --query 'status')
+            backupStatus=$(az backup protection check-vm --vm-id "/subscriptions/$subscription_id/resourceGroups/$resourceGroup/providers/Microsoft.Compute/virtualMachines/$vmName" --query 'status')
 
-            # Check if the VM is not backed up
-            if [ "$backupStatus" == "\"NotProtected\"" ]; then
-                echo "VM $vmName in resource group $resourceGroup is not backed up."
+            # Check if the VM backup is enabled
+            if [ "$backupStatus" == "\"Protected\"" ]; then
+                echo "$vmName,true,$subscription_name"
             else
-                echo "VM $vmName in resource group $resourceGroup is backed up."
+                echo "$vmName,false,$subscription_name"
             fi
-        done <<< "$vms"
-    done <<< "$resourceGroups"
-done <<< "$subscriptions"
+        done
--- vmwin2012script-csv-format.sh
+++ vmwin2012script-csv-format.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Output CSV header
-echo "VMName,BackupEnabled,SubscriptionName"
+echo "VMName,Offer,SKU,LicensePolicy,SubscriptionName"
 
 # Get the list of subscription IDs and names
 subscriptions=$(az account list --query "[].{id:id,name:name,state:state}" -o json | jq -r '.[] | select(.state == "Enabled") | "\(.id),\(.name)"')
@@ -9,30 +9,13 @@ subscriptions=$(az account list --query "[].{id:id,name:name,state:state}" -o js
 # Iterate over each subscription
 IFS=$'\n'
 for subscription in $subscriptions; do
-    subscription_id=$(echo "$subscription" | cut -d',' -f1)
-    subscription_name=$(echo "$subscription" | cut -d',' -f2)
+    subscription_id=$(echo $subscription | cut -d',' -f1)
+    subscription_name=$(echo $subscription | cut -d',' -f2)
 
     # Set the current subscription context
-    az account set --subscription "$subscription_id"
+    az account set --subscription $subscription_id
 
-    # Get a list of all resource groups in the current subscription
-    resourceGroups=$(az group list --query '[].name' --output tsv)
-
-    # Iterate through each resource group
-    while read -r resourceGroup; do
-        # Get a list of all VMs in the current resource group
-        vms=$(az vm list --resource-group "$resourceGroup" --query '[].name' --output tsv)
-
-        # Iterate through each VM
-        while read -r vmName; do
-            # Get backup status for the VM
-            backupStatus=$(az backup protection check-vm --vm-id "/subscriptions/$subscription_id/resourceGroups/$resourceGroup/providers/Microsoft.Compute/virtualMachines/$vmName" --query 'status')
-
-            # Check if the VM backup is enabled
-            if [ "$backupStatus" == "\"Protected\"" ]; then
-                echo "$vmName,true,$subscription_name"
-            else
-                echo "$vmName,false,$subscription_name"
-            fi
-        done
+    # Run the Azure CLI command for the current subscription and transform output to CSV
+    az vm list --query "[?storageProfile.imageReference.sku=='2012-Datacenter' || storageProfile.imageReference.sku=='2012-datacenter-gensecond' || storageProfile.imageReference.sku=='2012-Datacenter-smalldisk' || storageProfile.imageReference.sku=='2012-datacenter-smalldisk-g2' || storageProfile.imageReference.sku=='2012-Datacenter-zhcn' || storageProfile.imageReference.sku=='2012-datacenter-zhcn-g2' || storageProfile.imageReference.sku=='2012-R2-Datacenter' || storageProfile.imageReference.sku=='2012-r2-datacenter-gensecond' || storageProfile.imageReference.sku=='2012-R2-Datacenter-smalldisk' || storageProfile.imageReference.sku=='2012-r2-datacenter-smalldisk-g2' || storageProfile.imageReference.sku=='2012-R2-Datacenter-zhcn' || storageProfile.imageReference.sku=='2012-r2-datacenter-zhcn-g2' || storageProfile.imageReference.sku=='2008-Datacenter' || storageProfile.imageReference.sku=='2008-r2-datacenter' || storageProfile.imageReference.sku=='2008-Enterprise' || storageProfile.imageReference.sku=='2003-Enterprise'].{VMName:name,Offer:storageProfile.imageReference.offer,SKU:storageProfile.imageReference.sku, LicensePolicy:licenseType}" --output json | jq -r --arg subscription_name "$subscription_name" '.[] | "\(.VMName),\(.Offer),\(.SKU),\(.LicensePolicy),\($subscription_name)"'
+done | sed '/^\s*$/d' # Remove empty lines
 
