d556137709e66aa1206a6168895ec3e908315bb9
Bryan Carlos Martinez Elizalde <55309306+bryanmartineze@users.noreply.github.com>
2023-11-30 13:29:10 -0600

    script working

--- vmswithbackupscript-console-format.sh
+++ vmswithbackupscript-console-format.sh
@@ -25,13 +25,21 @@ for subscription in $subscriptions; do
 
         # Iterate through each VM
         while read -r vmName; do
-            # Get backup status for the VM
-            backupStatus=$(az backup protection check-vm --vm-id "/subscriptions/$subscription_id/resourceGroups/$resourceGroup/providers/Microsoft.Compute/virtualMachines/$vmName" --query 'status')
+            # Get the VM's service principal, if available
+            servicePrincipalId=$(az vm show --resource-group "$resourceGroup" --name "$vmName" --query 'identity.principalId' --output tsv)
 
-            # Check if the VM backup is enabled
-            if [ "$backupStatus" == "\"Protected\"" ]; then
+            # Check if the VM has the necessary permission to perform backup status action
+            if [ -n "$servicePrincipalId" ]; then
+                hasBackupPermission=$(az role assignment list --assignee "$servicePrincipalId" --scope "/subscriptions/$subscription_id/resourceGroups/$resourceGroup/providers/Microsoft.RecoveryServices/locations/backupStatus" --query '[].roleDefinitionName' --output tsv)
+            else
+                hasBackupPermission=""
+            fi
+
+            if [ -n "$hasBackupPermission" ]; then
                 echo "$vmName,true,$subscription_name"
             else
                 echo "$vmName,false,$subscription_name"
             fi
-        done
+        done <<< "$vms"
+    done <<< "$resourceGroups"
+done | sed '/^\s*$/d' # Remove empty lines
